691ad7cbad5dd52608cb12fffcdd09f9b78dc6f2
David Samuelsen <david@pfg.dk>
2018-05-25 03:15:24 +0200

    last try

--- src/components/MapCity.js
+++ src/components/MapCity.js
@@ -17,7 +17,7 @@ class MapCity extends Component {
             return (
                 <Map
                     isMarkerShown
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCu7L1Cc212OZKjqBpEStL6z97IGjLeWt8&v=3.exp&libraries=geometry,drawing,places"
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ width: '100%', height: `900px`, position: 'relative'}} />}
                     mapElement={<div style={{ height: `90%` }} />}
