bfddaadfbe225eba8d542cfb3a2153dc5e2c2af2
David Samuelsen <david@pfg.dk>
2018-05-25 02:24:13 +0200

    remove serviceworker

--- tests/seleniumtest.js
+++ tests/seleniumtest.js
@@ -3,9 +3,16 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get('http://167.99.237.199/');
+    
+
+    let mongo = await driver.findElement(By.id('mongo'))
+    mongo.click()
+
+    await driver.wait(until.elementIsSelected(mongo), 1000);
+    await driver.findElement(By.id("1")).click()
+    await driver.findElement(By.id("city")).sendKeys('webdriver', "Copenhagen");
+    await driver.findElement(By.id("run")).click()
   } finally {
     await driver.quit();
   }
